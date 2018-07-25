a41302cc37f889dd9ca61abf7fc02163a85430a3
Mobious <sammiel.gross@gmail.com>
2018-07-25 16:00:39 -0500

    css

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+// generate random number for target number
+// create 4 ruby elements to generate random number under 10
+// each ruby number adds to user number
+// if user number is greater than computer number, user loses
+// else user wins. Then new game restarts
